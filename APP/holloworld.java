269ae142b97ef3882a6f27cd62dc66378a1f600b
Xiang1032 <GitHub>
2026-03-05 11:38:28 +0800

    Initial merge of APP folder

---
+++ APP/holloworld.java
@@ -0,0 +1,12 @@
+public class holloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        int grade = 80;
+        if (grade >60 || grade == 60) {
+            System.out.println("成績及格\n" + grade);
+        } else {
+            System.out.println("fail");
+        }
+    }
+}
